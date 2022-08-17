86d0f2e3cd80f29fc39e2c9d9d5d72dea18288fb
Brandon <20081153@mail.wit.ie>
2022-08-16 23:39:53 -0400

    Update add city button and clean code

--- app/src/main/java/com/example/theweatheriswhat/ListViewActivity.java
+++ app/src/main/java/com/example/theweatheriswhat/ListViewActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,7 +16,7 @@ import org.w3c.dom.Text;
 import java.util.ArrayList;
 
 public class ListViewActivity extends AppCompatActivity {
-
+    EditText enterCityName;
     ListView lvCities;
     ArrayList<String> citiesArrayList = new ArrayList<>();
     ArrayAdapter<String> arrayAdapter;
@@ -26,11 +27,12 @@ public class ListViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_view);
 
         lvCities = findViewById(R.id.lvCities);
+        enterCityName = findViewById(R.id.etEnterCityName);
 
-        citiesArrayList.add("City A");
-        citiesArrayList.add("City B");
-        citiesArrayList.add("City C");
-        citiesArrayList.remove("City C");
+//        citiesArrayList.add("City A");
+//        citiesArrayList.add("City B");
+//        citiesArrayList.add("City C");
+//        citiesArrayList.remove("City C");
 //        citiesArrayList.clear();
 
         arrayAdapter = new ArrayAdapter<String>(ListViewActivity.this, android.R.layout.simple_dropdown_item_1line, citiesArrayList);
@@ -48,7 +50,8 @@ public class ListViewActivity extends AppCompatActivity {
     }
 
     public void AddCity(View view) {
-        citiesArrayList.add("dummy city");
+        String CityNameStr = enterCityName.getText().toString();
+        citiesArrayList.add(CityNameStr);
         arrayAdapter.notifyDataSetChanged();
     }
 }
