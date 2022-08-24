a345e91cae3ce5f382992f83f4cc71400200b07f
Brandon <20081153@mail.wit.ie>
2022-08-24 11:17:45 -0400

    Firebase Authentication Working

--- app/src/main/java/com/example/theweatheriswhat/MainActivity.java
+++ app/src/main/java/com/example/theweatheriswhat/MainActivity.java
@@ -2,51 +2,71 @@ package com.example.theweatheriswhat;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.theweatheriswhat.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.theweatheriswhat.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
     Button btnGoToCreateAccount, btnLogin;
     EditText etEnterEmail, etEnterPassword;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
         btnLogin = findViewById(R.id.btnLogin);
         btnGoToCreateAccount = findViewById(R.id.btnGoToCreateAccount);
         etEnterEmail = findViewById(R.id.etEnterEmail);
         etEnterPassword = findViewById(R.id.etEnterPassword);
 
-        btnGoToCreateAccount.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, CreateAccount.class);
-            startActivity(intent);
-        });
 
-        btnLogin.setOnClickListener(v -> {
+        btnGoToCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, CreateAccount.class);
+                startActivity(intent);
+            }
+            });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailStr = etEnterEmail.getText().toString();
+                String passStr = etEnterPassword.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(emailStr, passStr)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(MainActivity.this, "Login successful.",
+                                            Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(MainActivity.this, CitiesWeatherActivity.class);
             startActivity(intent);
-        });
-
+        } else {
+            Toast.makeText(MainActivity.this, "Login failed.",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
-}
+});
+        }
+        });
+        }
+        }
